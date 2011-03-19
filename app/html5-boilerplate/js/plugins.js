2ade4853b69849b7de8f63ea9bd556b95da847c1
Chris Hager <chager@myyearbook.com>
2011-03-18 21:31:29 -0400

    readded html5-boilerplate

---
+++ app/html5-boilerplate/js/plugins.js
@@ -0,0 +1,417 @@
+/*
+	Simple OpenID Plugin
+	http://code.google.com/p/openid-selector/
+	
+	This code is licensed under the New BSD License.
+*/
+
+var providers_large = {
+	google : {
+		name : 'Google',
+		url : 'https://www.google.com/accounts/o8/id'
+	},
+	yahoo : {
+		name : 'Yahoo',
+		url : 'http://me.yahoo.com/'
+	},
+	aol : {
+		name : 'AOL',
+		label : 'Enter your AOL screenname.',
+		url : 'http://openid.aol.com/{username}'
+	},
+	myopenid : {
+		name : 'MyOpenID',
+		label : 'Enter your MyOpenID username.',
+		url : 'http://{username}.myopenid.com/'
+	},
+	openid : {
+		name : 'OpenID',
+		label : 'Enter your OpenID.',
+		url : null
+	}
+};
+
+var providers_small = {
+	livejournal : {
+		name : 'LiveJournal',
+		label : 'Enter your Livejournal username.',
+		url : 'http://{username}.livejournal.com/'
+	},
+	/* flickr: {
+		name: 'Flickr',        
+		label: 'Enter your Flickr username.',
+		url: 'http://flickr.com/{username}/'
+	}, */
+	/* technorati: {
+		name: 'Technorati',
+		label: 'Enter your Technorati username.',
+		url: 'http://technorati.com/people/technorati/{username}/'
+	}, */
+	wordpress : {
+		name : 'Wordpress',
+		label : 'Enter your Wordpress.com username.',
+		url : 'http://{username}.wordpress.com/'
+	},
+	blogger : {
+		name : 'Blogger',
+		label : 'Your Blogger account',
+		url : 'http://{username}.blogspot.com/'
+	},
+	verisign : {
+		name : 'Verisign',
+		label : 'Your Verisign username',
+		url : 'http://{username}.pip.verisignlabs.com/'
+	},
+	/* vidoop: {
+		name: 'Vidoop',
+		label: 'Your Vidoop username',
+		url: 'http://{username}.myvidoop.com/'
+	}, */
+	/* launchpad: {
+		name: 'Launchpad',
+		label: 'Your Launchpad username',
+		url: 'https://launchpad.net/~{username}'
+	}, */
+	claimid : {
+		name : 'ClaimID',
+		label : 'Your ClaimID username',
+		url : 'http://claimid.com/{username}'
+	},
+	clickpass : {
+		name : 'ClickPass',
+		label : 'Enter your ClickPass username',
+		url : 'http://clickpass.com/public/{username}'
+	},
+	google_profile : {
+		name : 'Google Profile',
+		label : 'Enter your Google Profile username',
+		url : 'http://www.google.com/profiles/{username}'
+	}
+};
+
+openid.locale = 'en';
+openid.sprite = 'en'; // reused in german& japan localization
+openid.demo_text = 'In client demo mode. Normally would have submitted OpenID:';
+openid.signin_text = 'Sign-In';
+openid.image_title = 'log in with {provider}';
+
+ /*
+ * TipTip
+ * Copyright 2010 Drew Wilson
+ * www.drewwilson.com
+ * code.drewwilson.com/entry/tiptip-jquery-plugin
+ *
+ * Version 1.3   -   Updated: Mar. 23, 2010
+ *
+ * This Plug-In will create a custom tooltip to replace the default
+ * browser tooltip. It is extremely lightweight and very smart in
+ * that it detects the edges of the browser window and will make sure
+ * the tooltip stays within the current window size. As a result the
+ * tooltip will adjust itself to be displayed above, below, to the left 
+ * or to the right depending on what is necessary to stay within the
+ * browser window. It is completely customizable as well via CSS.
+ *
+ * This TipTip jQuery plug-in is dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+(function($){
+	$.fn.tipTip = function(options) {
+		var defaults = { 
+			activation: "hover",
+			keepAlive: false,
+			maxWidth: "200px",
+			edgeOffset: 3,
+			defaultPosition: "bottom",
+			delay: 400,
+			fadeIn: 200,
+			fadeOut: 200,
+			attribute: "title",
+			content: false, // HTML or String to fill TipTIp with
+		  	enter: function(){},
+		  	exit: function(){}
+	  	};
+	 	var opts = $.extend(defaults, options);
+	 	
+	 	// Setup tip tip elements and render them to the DOM
+	 	if($("#tiptip_holder").length <= 0){
+	 		var tiptip_holder = $('<div id="tiptip_holder" style="max-width:'+ opts.maxWidth +';"></div>');
+			var tiptip_content = $('<div id="tiptip_content"></div>');
+			var tiptip_arrow = $('<div id="tiptip_arrow"></div>');
+			$("body").append(tiptip_holder.html(tiptip_content).prepend(tiptip_arrow.html('<div id="tiptip_arrow_inner"></div>')));
+		} else {
+			var tiptip_holder = $("#tiptip_holder");
+			var tiptip_content = $("#tiptip_content");
+			var tiptip_arrow = $("#tiptip_arrow");
+		}
+		
+		return this.each(function(){	    
+			var org_elem = $(this);
+			if(opts.content){
+				var org_title = opts.content;
+			} else {
+				var org_title = org_elem.attr(opts.attribute);
+			}
+			if(org_title != ""){
+				if(!opts.content){
+					org_elem.removeAttr(opts.attribute); //remove original Attribute
+				}
+				var timeout_fadein = false;
+				var timeout_fadeout = false; /* hover hack */
+				var is_out = false; // has already hovered out, to prevent to always show tip  
+				window.tip_is_active = false;
+				
+				if(opts.activation == "hover"){
+					org_elem.hover(function(){
+					    is_out = false;
+        				// cancel a possible cancel-timeout
+    					if (timeout_fadeout){ 
+    					   clearTimeout(timeout_fadeout); 
+					   }					   
+    					if (window.tip_is_active) {
+    					    // tip already active somewhere else, move
+    					    activate_tiptip();
+					    } else { 
+    					   window.tip_is_active = true;
+    					   timeout_fadein = setTimeout(active_tiptip, opts.delay);
+                        }
+						//console.log("activate1");
+					}, function(){
+						if(!opts.keepAlive){
+    						//console.log("deactivate1");
+        					if (timeout_fadein){ 
+        					   clearTimeout(timeout_fadein); 
+    					   }
+    					   window.tip_is_active = false;
+    					   timeout_fadeout = setTimeout(function() { if (!window.tip_is_active) deactive_tiptip(); }, 1000);
+						}
+					});
+
+                    /* Tooltip itself */
+					tiptip_holder.hover(function(){ 
+					  /* hover in */
+					  clearTimeout(timeout_fadeout);  
+					 }, function(){  
+					    /* hover out */ 
+						deactive_tiptip();
+ 					 });
+				} else if(opts.activation == "focus"){
+					org_elem.focus(function(){
+						active_tiptip();
+					}).blur(function(){
+						deactive_tiptip();
+					});
+				} else if(opts.activation == "click"){
+					org_elem.click(function(){
+						active_tiptip();
+						return false;
+					}).hover(function(){},function(){
+						if(!opts.keepAlive){
+							deactive_tiptip();
+						}
+					});
+					if(opts.keepAlive){
+						tiptip_holder.hover(function(){}, function(){
+							deactive_tiptip();
+						});
+					}
+				}
+			
+				function active_tiptip(){
+					opts.enter.call(this);
+					tiptip_content.html(org_title);
+					tiptip_holder.hide().removeAttr("class").css("margin","0");
+					tiptip_arrow.removeAttr("style");
+					
+					var top = parseInt(org_elem.offset()['top']);
+					var left = parseInt(org_elem.offset()['left']);
+					var org_width = parseInt(org_elem.outerWidth());
+					var org_height = parseInt(org_elem.outerHeight());
+					var tip_w = tiptip_holder.outerWidth();
+					var tip_h = tiptip_holder.outerHeight();
+					var w_compare = Math.round((org_width - tip_w) / 2);
+					var h_compare = Math.round((org_height - tip_h) / 2);
+					var marg_left = Math.round(left + w_compare);
+					var marg_top = Math.round(top + org_height + opts.edgeOffset);
+					var t_class = "";
+					var arrow_top = "";
+					var arrow_left = Math.round(tip_w - 12) / 2;
+
+                    if(opts.defaultPosition == "bottom"){
+                    	t_class = "_bottom";
+                   	} else if(opts.defaultPosition == "top"){ 
+                   		t_class = "_top";
+                   	} else if(opts.defaultPosition == "left"){
+                   		t_class = "_left";
+                   	} else if(opts.defaultPosition == "right"){
+                   		t_class = "_right";
+                   	}
+					
+					var right_compare = (w_compare + left) < parseInt($(window).scrollLeft());
+					var left_compare = (tip_w + left) > parseInt($(window).width());
+					
+					if((right_compare && w_compare < 0) || (t_class == "_right" && !left_compare) || (t_class == "_left" && left < (tip_w + opts.edgeOffset + 5))){
+						t_class = "_right";
+						arrow_top = Math.round(tip_h - 13) / 2;
+						arrow_left = -12;
+						marg_left = Math.round(left + org_width + opts.edgeOffset);
+						marg_top = Math.round(top + h_compare);
+					} else if((left_compare && w_compare < 0) || (t_class == "_left" && !right_compare)){
+						t_class = "_left";
+						arrow_top = Math.round(tip_h - 13) / 2;
+						arrow_left =  Math.round(tip_w);
+						marg_left = Math.round(left - (tip_w + opts.edgeOffset + 5));
+						marg_top = Math.round(top + h_compare);
+					}
+
+					var top_compare = (top + org_height + opts.edgeOffset + tip_h + 8) > parseInt($(window).height() + $(window).scrollTop());
+					var bottom_compare = ((top + org_height) - (opts.edgeOffset + tip_h + 8)) < 0;
+					
+					if(top_compare || (t_class == "_bottom" && top_compare) || (t_class == "_top" && !bottom_compare)){
+						if(t_class == "_top" || t_class == "_bottom"){
+							t_class = "_top";
+						} else {
+							t_class = t_class+"_top";
+						}
+						arrow_top = tip_h;
+						marg_top = Math.round(top - (tip_h + 5 + opts.edgeOffset));
+					} else if(bottom_compare | (t_class == "_top" && bottom_compare) || (t_class == "_bottom" && !top_compare)){
+						if(t_class == "_top" || t_class == "_bottom"){
+							t_class = "_bottom";
+						} else {
+							t_class = t_class+"_bottom";
+						}
+						arrow_top = -12;						
+						marg_top = Math.round(top + org_height + opts.edgeOffset);
+					}
+				
+					if(t_class == "_right_top" || t_class == "_left_top"){
+						marg_top = marg_top + 5;
+					} else if(t_class == "_right_bottom" || t_class == "_left_bottom"){		
+						marg_top = marg_top - 5;
+					}
+					if(t_class == "_left_top" || t_class == "_left_bottom"){	
+						marg_left = marg_left + 5;
+					}
+					tiptip_arrow.css({"margin-left": arrow_left+"px", "margin-top": arrow_top+"px"});
+					tiptip_holder.css({"margin-left": marg_left+"px", "margin-top": marg_top+"px"}).attr("class","tip"+t_class);
+					
+					tiptip_holder.stop(true,true).fadeIn(opts.fadeIn);	
+				}
+				
+				function deactive_tiptip(){
+					//console.log("deactivate++++");
+					opts.exit.call(this);
+					if (timeout_fadein){ clearTimeout(timeout_fadein); }
+					if (timeout_fadeout){ clearTimeout(timeout_fadeout); }
+					tiptip_holder.fadeOut(opts.fadeOut);
+				}
+			}				
+		});
+	}
+})(jQuery);  	
+
+
+
+(function( $ ) {
+	$.widget( "ui.combobox", {
+		_create: function() {
+			var self = this,
+				select = this.element.hide(),
+				selected = select.children( ":selected" ),
+				value = selected.val() ? selected.text() : "";
+			var input = this.input = $( "<input>" )
+				.insertAfter( select )
+				.val( value )
+				.autocomplete({
+					delay: 0,
+					minLength: 0,
+					source: function( request, response ) {
+						var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+						response( select.children( "option" ).map(function() {
+							var text = $( this ).text();
+							if ( this.value && ( !request.term || matcher.test(text) ) )
+								return {
+									label: text.replace(
+										new RegExp(
+											"(?![^&;]+;)(?!<[^<>]*)(" +
+											$.ui.autocomplete.escapeRegex(request.term) +
+											")(?![^<>]*>)(?![^&;]+;)", "gi"
+										), "<strong>$1</strong>" ),
+									value: text,
+									option: this
+								};
+						}) );
+					},
+					select: function( event, ui ) {
+						ui.item.option.selected = true;
+						self._trigger( "selected", event, {
+							item: ui.item.option
+						});
+					},
+					change: function( event, ui ) {
+						if ( !ui.item ) {
+							var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i" ),
+								valid = false;
+							select.children( "option" ).each(function() {
+								if ( $( this ).text().match( matcher ) ) {
+									this.selected = valid = true;
+									return false;
+								}
+							});
+							if ( !valid ) {
+								// remove invalid value, as it didn't match anything
+								$( this ).val( "" );
+								select.val( "" );
+								input.data( "autocomplete" ).term = "";
+								return false;
+							}
+						}
+					}
+				})
+				.addClass( "ui-widget ui-widget-content ui-corner-left" );
+
+			input.data( "autocomplete" )._renderItem = function( ul, item ) {
+				return $( "<li></li>" )
+					.data( "item.autocomplete", item )
+					.append( "<a>" + item.label + "</a>" )
+					.appendTo( ul );
+			};
+
+			this.button = $( "<button type='button'>&nbsp;</button>" )
+				.attr( "tabIndex", -1 )
+				.attr( "title", "Show All Items" )
+				.insertAfter( input )
+				.button({
+					icons: {
+						primary: "ui-icon-triangle-1-s"
+					},
+					text: false
+				})
+				.removeClass( "ui-corner-all" )
+				.addClass( "ui-corner-right ui-button-icon ui-combobox-button" )
+				.click(function() {
+					// close if already visible
+					if ( input.autocomplete( "widget" ).is( ":visible" ) ) {
+						input.autocomplete( "close" );
+						return;
+					}
+
+					// pass empty string as value to search for, displaying all results
+					input.autocomplete( "search", "" );
+					input.focus();
+				});
+		},
+
+		destroy: function() {
+			this.input.remove();
+			this.button.remove();
+			this.element.show();
+			$.Widget.prototype.destroy.call( this );
+		}
+	});
+})( jQuery );
+
+
+
---
+++ app/html5-boilerplate/js/script.js
@@ -0,0 +1,138 @@
+function init() {
+    $("#q").click(function(){ if (this.value == "search") this.value=""; });
+
+	$(".btn").button();
+	$(".btn_small").button();
+}
+
+var is_editor = false; // toggled true <-> false
+var is_wmd = false;    // set to true only once
+
+function ui_to_editor() {
+    $("#view_title").hide();
+    $("#edit_title").show();
+    //$("#view_description").hide();
+    $("#view_description").hide();
+    $("#edit_description").show();
+    $("#code").hide();
+    $("#edit_code").show();
+    $("#editbox").show();
+    $("#subtitle").hide();
+    $("#comments").hide();
+
+    if (!is_wmd) {
+	    $("#desc").wmd({
+	        "preview": true,
+	        "helpLink": "/about/markdown",
+	        "helpHoverTitle": "Markdown Help",
+	    });       
+	    is_wmd = true;
+    }     
+    
+    $("#fdbk_tab").hide();    
+    $("#btn_edit .ui-button-text").html("Cancel");
+}
+
+function ui_to_viewmode() {
+    $("#view_title").show();
+    $("#edit_title").hide();
+    $("#view_description").show();
+    $("#edit_description").hide();
+    $("#code").show();
+    $("#editbox").hide();
+    $("#edit_code").hide();
+    $("#subtitle").show();
+    $("#comments").show();
+    
+    $("#fdbk_tab").show();    
+    $("#btn_edit .ui-button-text").html("Edit");
+}
+
+function _toggle_ui_to_editor() {
+    if (is_editor)
+        ui_to_viewmode();
+    else
+        ui_to_editor();
+    is_editor = !is_editor;
+}
+
+var previewWindow;    
+function preview() {
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "/preview");
+    
+    // setting form target to a window named 'formresult'
+    form.setAttribute("target", "previewWindow");
+    form.style.display = "none";
+    
+    var hiddenField = document.createElement("input");              
+    hiddenField.setAttribute("name", "title");
+    hiddenField.setAttribute("value", encodeURI($("#title").val()));
+    form.appendChild(hiddenField);
+
+    var hiddenField = document.createElement("input");              
+    hiddenField.setAttribute("name", "desc");
+    hiddenField.setAttribute("value", encodeURI($("#desc").val()));
+    form.appendChild(hiddenField);
+
+    var hiddenField = document.createElement("input");              
+    hiddenField.setAttribute("name", "code");
+    hiddenField.setAttribute("value", encodeURI($("#input_code").val()));
+    form.appendChild(hiddenField);
+
+    document.body.appendChild(form);
+        
+    if (!previewWindow || previewWindow.closed) {
+        // opens a new window with the edit
+        previewWindow = window.open("/preview", 'previewWindow',
+        'left=20,top=20,width=600,height=500,toolbar=0,location=0,resizable=0,menubar=0,status=0');
+    } else {
+        previewWindow.focus();
+    }
+
+    form.submit();
+}
+
+
+function _follow() {
+    alert("follow");
+}
+
+
+function feedback() {
+	// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+	$( "#dialog:ui-dialog" ).dialog( "destroy" );
+
+    $("#feedback-dialog-content").show();
+    $("#feedback-dialog-content-wait").hide();
+    $("#feedback-dialog-content-postsubmit").hide();        
+
+	$( "#feedback-dialog" ).dialog({
+		modal: true,
+        position:["center", 160], 
+        width:500, 
+	});
+}
+
+function feedback_submit() {
+    $("#feedback-dialog-content").hide();
+    $("#feedback-dialog-content-wait").show();
+    $("#feedback-dialog-content-postsubmit").hide();        
+
+    msg = { 
+        "msg": $("#feedback_text").val(),
+        "email": $("#feedback_email").val()
+    }
+    $.ajax({
+        type: 'POST',
+        url: "/about",
+        data: msg,
+        success: function(){
+            $("#feedback-dialog-content-wait").hide();
+            $("#feedback-dialog-content-postsubmit").show();        
+        }
+    });
+    
+    return false;
+}
