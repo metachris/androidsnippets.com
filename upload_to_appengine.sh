659cd77ff64c352b7cb6a65ceb87778075dc77ab
Chris Hager <chager@myyearbook.com>
2011-03-19 01:55:38 -0400

    updated upload shell script

--- upload_to_appengine.sh
+++ upload_to_appengine.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
-set -e
+
+DIR=$( pwd )
 
 function static_revert {
   # Set /static back to dev environment
+  set -e
   echo "Setting /static back to /html5-boilerplate"
   cd app
   rm static
   ln -s html5-boilerplate static
+  cd "$DIR"
+  set +e
 }
 
 function static_toprod {
-  echo "Setting /static to /html5-boilerplate/publish"
   # Set build script results as /static
+  set -e
+  echo "Setting /static to /html5-boilerplate/publish"
   cd app
   rm static
   ln -s html5-boilerplate/publish static
-  cd ..
+  cd "$DIR"
+  set +e
 }
 
 function upload {
   # Upload to appengine
-  set +e
   ~/Tools/google_appengine/appcfg.py update app
-  set -e
 }
 
 function build {
   cd app/html5-boilerplate/build
   ant minify
+  cd "$DIR"
 }
 
 read -p "Build the project with 'ant minify' now? [yN]" yn
@@ -38,7 +43,7 @@ esac
 
 static_toprod
 
-read -p "You can test the final version now locally. Do you wish to upload this program? [yN]" yn
+read -p "You can now test the latest build. Do you wish to upload this version? [yN]" yn
   case $yn in
     [Yy]* ) upload;;
     [Nn]* ) break;;
